0d99693bf991b08edc5ddd7e482204bcf9a9e850
Elliott Biondo <elliott.biondo@gmail.com>
2014-07-20 01:38:47 -0400

    It works, (all pyne tests pass) but it probably needs a final test.

--- vb-build.sh
+++ vb-build.sh
@@ -2,19 +2,33 @@
 set -euo pipefail
 IFS=$'\n\t'
 export buildlog=$HOME/buildlog
-echo "Starting build" > buildlog
-sudo apt-get install python-numpy python-scipy cython python-nose git cmake
+#export hdf5dir=/home/pyne-user/opt/hdf5-1.8.12-linux-x86_64-shared
+export hdf5dir=/home/pyne-user/opt/hdf5
+echo "Starting build" > $buildlog
+sudo apt-get install python-numpy python-scipy cython python-nose git cmake vim emacs
 echo "apt" >> $buildlog
 cd $HOME
 mkdir opt
 cd opt
-wget http://www.hdfgroup.org/ftp/HDF5/current/bin/linux-x86_64/hdf5-1.8.13-linux-x86_64-shared.tar.gz
-tar zxvf hdf5-1.8.13-linux-x86_64-shared.tar.gz
-rm hdf5-1.8.13-linux-x86_64-shared.tar.gz
-echo "export LD_LIBRARY_PATH=/home/pyneuser/opt/hdf5-1.8.13-linux-x86_64-shared/lib:$LD_LIBRARY_PATH" >> ~/.bashrc
-echo "export LIBRARY_PATH=/home/pyneuser/opt/hdf5-1.8.13-linux-x86_64-shared/lib:$LIBRARY_PATH" >> ~/.bashrc
-echo "export CPLUS_INCLUDE_PATH=/home/pyneuser/opt/hdf5-1.8.13-linux-x86_64-shared/include:$CPLUS_INCLUDE_PATH" >> ~/.bashrc
-echo "export C_INCLUDE_PATH=/home/pyneuser/opt/hdf5-1.8.13-linux-x86_64-shared/include:$C_INCLUDE_PATH" >> ~/.bashrc
+#echo "slib" >> $buildlog
+#wget http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.12/bin/linux-x86_64/hdf5-1.8.12-linux-x86_64-shared.tar.gz
+#tar zxvf hdf5-1.8.12-linux-x86_64-shared.tar.gz
+#rm hdf5-1.8.12-linux-x86_64-shared.tar.gz
+mkdir hdf5
+cd hdf5
+wget http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.11/src/hdf5-1.8.11.tar.gz
+tar zxvf hdf5-1.8.11.tar.gz
+rm hdf5-1.8.11.tar.gz
+mkdir build
+cd build
+../hdf5-1.8.11/configure --enable-shared --prefix=$HOME/opt/hdf5 --enable-cxx
+make
+make install
+cd ../../
+echo "export LD_LIBRARY_PATH=$hdf5dir/lib:\$LD_LIBRARY_PATH" >> ~/.bashrc
+echo "export LIBRARY_PATH=$hdf5dir/lib:\$LIBRARY_PATH" >> ~/.bashrc
+echo "export CPLUS_INCLUDE_PATH=$hdf5dir/include:\$CPLUS_INCLUDE_PATH" >> ~/.bashrc
+echo "export C_INCLUDE_PATH=$hdf5dir/include:\$C_INCLUDE_PATH" >> ~/.bashrc
 source ~/.bashrc
 echo "hdf5" >> $buildlog
 git clone https://github.com/pydata/numexpr.git
@@ -24,7 +38,7 @@ cd ..
 echo "numexpr" >> $buildlog
 git clone https://github.com/PyTables/PyTables.git
 cd PyTables
-python setup.py install --user --hdf5=/home/pyneuser/opt/hdf5-1.8.13-linux-x86_64-shared
+python setup.py install --user --hdf5=/home/pyne-user/opt/hdf5
 cd ..
 echo "pytables" >> $buildlog
 mkdir moab
@@ -34,13 +48,13 @@ tar zxvf moab-4.6.2.tar.gz
 rm moab-4.6.2.tar.gz
 mkdir build
 cd build
-../moab-4.6.2/configure --enable-shared --with-hdf5=/home/pyneuser/opt/hdf5-1.8.13-linux-x86_64-shared --prefix=/home/pyneuser/.local
+../moab-4.6.2/configure --enable-shared --with-hdf5=/home/pyne-user/opt/hdf5 --prefix=/home/pyne-user/.local
 make
 make install
-echo "export LD_LIBRARY_PATH=/home/pyneuser/.local/lib:$LD_LIBRARY_PATH" >> ~/.bashrc
-echo "export LIBRARY_PATH=/home/pyneuser/.local/lib:$LIBRARY_PATH" >> ~/.bashrc
-echo "export CPLUS_INCLUDE_PATH=/home/pyneuser/.local/include:$CPLUS_INCLUDE_PATH" >> ~/.bashrc
-echo "export C_INCLUDE_PATH=/home/pyneuser/.local/include:$C_INCLUDE_PATH" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=/home/pyne-user/.local/lib:\$LD_LIBRARY_PATH" >> ~/.bashrc
+echo "export LIBRARY_PATH=/home/pyne-user/.local/lib:\$LIBRARY_PATH" >> ~/.bashrc
+echo "export CPLUS_INCLUDE_PATH=/home/pyne-user/.local/include:\$CPLUS_INCLUDE_PATH" >> ~/.bashrc
+echo "export C_INCLUDE_PATH=/home/pyne-user/.local/include:\$C_INCLUDE_PATH" >> ~/.bashrc
 source ~/.bashrc
 echo "moab" >> $buildlog
 cd ../../
@@ -48,12 +62,18 @@ wget https://pypi.python.org/packages/source/P/PyTAPS/PyTAPS-1.4.tar.gz
 tar zxvf PyTAPS-1.4.tar.gz
 rm PyTAPS-1.4.tar.gz
 cd PyTAPS-1.4/
-python setup.py install --imesh-path=/home/pyneuser/.local/ --user
+python setup.py --iMesh-path=/home/pyne-user/.local/ install --user
 echo "pytaps" >> $buildlog
 cd ..
 git clone https://github.com/pyne/pyne.git
 cd pyne
 python setup.py install --user
+echo "export PATH=/home/pyne-user/.local/bin:\$PATH" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=/home/pyne-user/.local/lib:\$LD_LIBRARY_PATH" >> ~/.bashrc
+source ~/.bashrc
+cd scripts
+./nuc_data_make
+cd ..
 cd tests
 nosetests
 echo "pyne" >> $buildlog
