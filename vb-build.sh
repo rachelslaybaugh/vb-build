a3048146193ab15a9e37c8cbb98de5e2a46ae719
Elliott Biondo <biondo@wisc.edu>
2014-08-25 16:39:52 -0500

    HDF5 from package manager

--- vb-build.sh
+++ vb-build.sh
@@ -4,29 +4,13 @@ IFS=$'\n\t'
 export buildlog=$HOME/buildlog
 export hdf5dir=$HOME/opt/hdf5
 echo "Starting build" > $buildlog
-sudo apt-get install -y python-numpy python-scipy cython python-nose git cmake vim emacs gfortran libblas-dev liblapack-dev
+sudo apt-get install -y python-numpy python-scipy cython python-nose git cmake vim emacs gfortran libblas-dev liblapack-dev libhdf5-dev
+export LD_LIBARY_PATH=/usr/lib/x86_64-linux-gnu
+echo "export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu" >> ~/.bashrc
 echo "apt" >> $buildlog
 cd $HOME
 mkdir opt
 cd opt
-mkdir hdf5
-cd hdf5
-wget http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.11/src/hdf5-1.8.11.tar.gz
-tar zxvf hdf5-1.8.11.tar.gz
-rm hdf5-1.8.11.tar.gz
-mkdir build
-cd build
-../hdf5-1.8.11/configure --enable-shared --prefix=$HOME/opt/hdf5 --enable-cxx
-make
-make install
-cd ../../
-export LD_LIBRARY_PATH=$hdf5dir/lib:\$LD_LIBRARY_PATH
-export LIBRARY_PATH=$hdf5dir/lib:\$LIBRARY_PATH
-echo "export LD_LIBRARY_PATH=$hdf5dir/lib:\$LD_LIBRARY_PATH" >> ~/.bashrc
-echo "export LIBRARY_PATH=$hdf5dir/lib:\$LIBRARY_PATH" >> ~/.bashrc
-echo "export CPLUS_INCLUDE_PATH=$hdf5dir/include:\$CPLUS_INCLUDE_PATH" >> ~/.bashrc
-echo "export C_INCLUDE_PATH=$hdf5dir/include:\$C_INCLUDE_PATH" >> ~/.bashrc
-echo "hdf5" >> $buildlog
 git clone https://github.com/pydata/numexpr.git
 cd numexpr
 python setup.py install --user
@@ -34,7 +18,7 @@ cd ..
 echo "numexpr" >> $buildlog
 git clone https://github.com/PyTables/PyTables.git
 cd PyTables
-python setup.py install --user --hdf5=$HOME/opt/hdf5
+python setup.py install --user
 cd ..
 echo "pytables" >> $buildlog
 mkdir moab
@@ -44,7 +28,7 @@ tar zxvf moab-4.6.2.tar.gz
 rm moab-4.6.2.tar.gz
 mkdir build
 cd build
-../moab-4.6.2/configure --enable-shared --with-hdf5=$HOME/opt/hdf5 --prefix=$HOME/.local
+../moab-4.6.2/configure --enable-shared --prefix=$HOME/.local
 make
 make install
 export LD_LIBRARY_PATH=$HOME/.local/lib:\$LD_LIBRARY_PATH
