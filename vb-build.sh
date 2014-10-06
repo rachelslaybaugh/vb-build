64fd56699ea65ba275a73c6a45e2867eae605219
rachelslaybaugh <r.slaybaugh@gmail.com>
2014-10-06 14:15:41 -0700

    a plausibly working version of this script on my machine. had to uninstall conda's version of yt.

--- vb-build.sh
+++ vb-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script builds the repo version of PyNE (with the MOAB optional 
-# dependency) from scratch on Ubuntu 14.04. The folder $HOME/opt is created 
-# and PyNE is installed within.
+# dependency) from scratch on Ubuntu 14.04. The folder /home/Install/PyNE
+# is created and PyNE is installed within.
 #
 # Run this script from any directory by issuing the command:
 # $ ./vb-build
@@ -10,49 +10,69 @@
 # or open a new terminal.
 set -euo pipefail
 IFS=$'\n\t'
+
 # Use package manager for as many packages as possible
-sudo apt-get install -y python-numpy python-scipy cython python-nose git cmake vim emacs gfortran libblas-dev liblapack-dev libhdf5-dev gfortran liblapack-dev libblas-dev python-tables
+#sudo apt-get install -y python-numpy python-scipy cython python-nose git cmake vim emacs gfortran libblas-dev liblapack-dev libhdf5-dev python-tables
 # need to put libhdf5.so on LD_LIBRARY_PATH
+# Note: I needed to add H5FDmpiposix.h to /home/Install/anaconda/include for
+# this to work. I got it from:
+#http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.1/src/unpacked/src/H5FDmpiposix.h
+
 export LD_LIBARY_PATH=/usr/lib/x86_64-linux-gnu
 echo "export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu" >> ~/.bashrc
-cd $HOME
-mkdir opt
-cd opt
+
+export INSTALL_BASE=/home/Install
+export INSTALL_DIR=$INSTALL_BASE/PyNE
+export BUILD_DIR=/home/slayer/Software/Sources
+export PYNE_DIR=/home/slayer/Software/PyNE/pyne
+
 # Install MOAB
-mkdir moab
+cd $BUILD_DIR
+#mkdir moab
 cd moab
-wget http://ftp.mcs.anl.gov/pub/fathom/moab-4.6.2.tar.gz
-tar zxvf moab-4.6.2.tar.gz
-rm moab-4.6.2.tar.gz
-mkdir build
+#wget http://ftp.mcs.anl.gov/pub/fathom/moab-4.6.2.tar.gz
+#tar zxvf moab-4.6.2.tar.gz
+#rm moab-4.6.2.tar.gz
+
+#mkdir build
 cd build
-../moab-4.6.2/configure --enable-shared --prefix=$HOME/.local
-make
+../moab-4.6.2/configure --enable-shared --prefix=$INSTALL_DIR
+make clean
+make -j 8
 make install
-export LD_LIBRARY_PATH=$HOME/.local/lib:\$LD_LIBRARY_PATH
-export LIBRARY_PATH=$HOME/.local/lib:\$LIBRARY_PATH
-echo "export LD_LIBRARY_PATH=$HOME/.local/lib:\$LD_LIBRARY_PATH" >> ~/.bashrc
-echo "export LIBRARY_PATH=$HOME/.local/lib:\$LIBRARY_PATH" >> ~/.bashrc
-echo "export CPLUS_INCLUDE_PATH=$HOME/.local/include:\$CPLUS_INCLUDE_PATH" >> ~/.bashrc
-echo "export C_INCLUDE_PATH=$HOME/.local/include:\$C_INCLUDE_PATH" >> ~/.bashrc
-cd ../../
+
+export LD_LIBRARY_PATH=$INSTALL_DIR/lib:$LD_LIBRARY_PATH
+export LIBRARY_PATH=$INSTALL_DIR/lib:$LIBRARY_PATH
+export CPLUS_INCLUDE_PATH=$INSTALL_DIR/include:$CPLUS_INCLUDE_PATH
+export C_INCLUDE_PATH=$INSTALL_DIR/include:$C_INCLUDE_PATH
+export LD_LIBRARY_PATH=/home/Install/anaconda/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/slayer/.local/lib:$LD_LIBRARY_PATH
+echo "export LD_LIBRARY_PATH=$INSTALL_DIR/lib:$LD_LIBRARY_PATH" >> ~/.bashrc
+echo "export LIBRARY_PATH=$INSTALL_DIR/lib:$LIBRARY_PATH" >> ~/.bashrc
+echo "export CPLUS_INCLUDE_PATH=$INSTALL_DIR/include:$CPLUS_INCLUDE_PATH" >> ~/.bashrc
+echo "export C_INCLUDE_PATH=$INSTALL_DIR/include:$C_INCLUDE_PATH" >> ~/.bashrc
+
 # Install PyTAPS
-wget https://pypi.python.org/packages/source/P/PyTAPS/PyTAPS-1.4.tar.gz
-tar zxvf PyTAPS-1.4.tar.gz
-rm PyTAPS-1.4.tar.gz
+cd $BUILD_DIR
+#mkdir pytaps
+cd pytaps
+#wget https://pypi.python.org/packages/source/P/PyTAPS/PyTAPS-1.4.tar.gz
+#tar zxvf PyTAPS-1.4.tar.gz
+#rm PyTAPS-1.4.tar.gz
 cd PyTAPS-1.4/
-python setup.py --iMesh-path=$HOME/.local/ install --user
-cd ..
+python setup.py --iMesh-path=$INSTALL_DIR install --user
+
 # Install PyNE
 #git clone https://github.com/pyne/pyne.git
-cd $HOME/Software/PyNE/pyne
-python setup.py install --hdf5=$HOME/opt/hdf5 --user
-echo "export PATH=$HOME/.local/bin:\$PATH" >> ~/.bashrc
-echo "export LD_LIBRARY_PATH=$HOME/.local/lib:\$LD_LIBRARY_PATH" >> ~/.bashrc
+cd $PYNE_DIR
+python setup.py install --hdf5=/home/Install/anaconda --user
+echo "export PATH=$INSTALL_DIR/bin:$PATH" >> ~/.bashrc
+
 # Generate nuclear data file
 cd scripts
 ./nuc_data_make
 cd ..
+
 # Run all the tests
 cd tests
 nosetests
